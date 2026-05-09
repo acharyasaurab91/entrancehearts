5fee1c0f3adcd8d8a730e98ffbae1eb5dbdbab2c
acharyasaurab91 <acharyasaurab91@gmail.com>
2026-05-09 16:34:51 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -40,3 +40,29 @@ self.addEventListener('fetch', e => {
   }
 });
 
+// Add to your sw.js
+self.addEventListener('push', function(event) {
+  const data = event.data.json();
+  const options = {
+    body: data.body,
+    icon: '/icon-192.png',
+    badge: '/icon-192.png',
+    vibrate: [200, 100, 200],
+    data: {
+      url: data.url
+    }
+  };
+  
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  if (event.notification.data && event.notification.data.url) {
+    event.waitUntil(
+      clients.openWindow(event.notification.data.url)
+    );
+  }
+});
